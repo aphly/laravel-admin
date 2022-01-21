8f856fe0d8667e6064579fb7d65f29ea4796de98
Aphly <121099327@qq.com>
2022-01-21 16:17:08 +0800

    update

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Aphly\LaravelAdmin;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+
+    public function register()
+    {
+		$this->mergeConfigFrom(
+            __DIR__.'/config/admin.php', 'admin'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/admin.php' => config_path('admin.php'),
+        ]);
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        $this->loadViewsFrom(__DIR__.'/views', 'admin');
+    }
+
+
+}
---
+++ src/migrations/2022_01_21_000000_manager.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Manager extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manager', function (Blueprint $table) {
+            $table->id();
+            $table->char('name',32)->unique();
+            $table->string('username',32);
+            $table->string('nickname',32);
+            $table->string('email',128);
+            $table->string('phone',16);
+            $table->string('password');
+            $table->string('token')->unique();
+            $table->integer('token_expire')->unsigned();
+            $table->string('avatar',255);
+            $table->tinyInteger('status');
+            $table->timestamp('last_login');
+            $table->string('last_ip',64);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manager');
+    }
+}
