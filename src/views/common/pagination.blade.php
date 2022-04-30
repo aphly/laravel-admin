b4326f580d20ed9b07e0a6cb5c630ff9ac2f142f
aphly <121099327@qq.com>
2022-04-30 19:40:14 +0800

    update

--- src/views/common/pagination.blade.php
+++ src/views/common/pagination.blade.php
@@ -2,9 +2,9 @@
     <ul class="pagination">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="page-item disabled"><span class="page-link">第一页</span></li>
+            <li class="page-item disabled page_pre"><span class="page-link">第一页</span></li>
         @else
-            <li class="page-item"><a class="page-link" data-href="{{ $paginator->previousPageUrl() }}"
+            <li class="page-item page_pre"><a class="page-link" data-href="{{ $paginator->previousPageUrl() }}"
                                      href="{{ $paginator->previousPageUrl() }}" rel="prev">上一页</a></li>
         @endif
 
@@ -33,10 +33,10 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="page-item"><a class="page-link" data-href="{{ $paginator->nextPageUrl() }}"
+            <li class="page-item page_next"><a class="page-link" data-href="{{ $paginator->nextPageUrl() }}"
                                      href="{{ $paginator->nextPageUrl() }}" rel="next">下一页</a></li>
         @else
-            <li class="page-item disabled"><span
+            <li class="page-item disabled page_next"><span
                     class="page-link">最后一页</span></li>
         @endif
     </ul>
