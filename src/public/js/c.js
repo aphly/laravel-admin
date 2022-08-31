1f4e1b69001913758a0f7dac17f10b97192cf3d2
Aphly <121099327@qq.com>
2022-08-31 17:33:37 +0800

    update

--- src/public/js/c.js
+++ src/public/js/c.js
@@ -203,4 +203,58 @@ function randomId(n,all=false) {
     return res;
 }
 
+window.onload = function() {
+    const bowen = document.querySelectorAll(".bowen")
+    bowen.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            let x = e.clientX - e.target.offsetLeft;
+            let y = e.clientY - e.target.offsetTop;
 
+            let ripples = document.createElement('span');
+            ripples.style.left = x + 'px';
+            ripples.style.top = y + 'px';
+            this.appendChild(ripples);
+            setTimeout(() => {
+                ripples.remove();
+            }, 400);
+        })
+    })
+}
+
+let mobileTouch = {
+    getDirection(startX, startY, endX, endY) {
+        let dy = startY - endY;
+        let result = 0;
+        if (dy > 0) { //向上滑动
+            result = 1;
+        } else if (dy < 0) {//向下滑动
+            result = 2;
+        } else {
+            result = 0;
+        }
+        return result;
+    },
+    listentouch(callback) {
+        let startX, startY;
+        document.addEventListener(
+            "touchstart",
+            function(ev) {
+                startX = ev.touches[0].pageX;
+                startY = ev.touches[0].pageY;
+            },
+            false
+        );
+        let _this = this
+        document.addEventListener(
+            "touchend",
+            function(ev) {
+                let endX, endY;
+                endX = ev.changedTouches[0].pageX;
+                endY = ev.changedTouches[0].pageY;
+                let direction = _this.getDirection(startX, startY, endX, endY);
+                callback(direction);
+            },
+            false
+        );
+    }
+}
